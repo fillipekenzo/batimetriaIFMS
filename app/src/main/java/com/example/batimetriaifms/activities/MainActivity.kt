30683e0d4a4b7511972ed391bc575a3178933359
Fillipe kenzo <fillipekenzo@hotmail.com>
2019-10-17 17:40:50 -0400

    Refatorando pagina Login

--- app/src/main/java/com/example/batimetriaifms/activities/MainActivity.kt
+++ app/src/main/java/com/example/batimetriaifms/activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.batimetriaifms
+package com.example.batimetriaifms.activities
 
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -13,6 +13,7 @@ import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.view.Menu
+import com.example.batimetriaifms.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,8 +37,11 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send
+                R.id.nav_home,
+                R.id.nav_gallery,
+                R.id.nav_tools,
+                R.id.nav_share,
+                R.id.nav_send
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
