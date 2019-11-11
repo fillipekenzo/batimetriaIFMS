5c67ebf694b7690e03d948394bfc5724b1c8eaef
Fillipe Kenzo <fillipekenzo123@gmail.com>
2019-11-11 19:01:44 -0400

    firebase Login

--- app/src/main/java/com/example/batimetriaifms/activities/MainActivity.kt
+++ app/src/main/java/com/example/batimetriaifms/activities/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.batimetriaifms.activities
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -14,21 +16,46 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.view.Menu
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.view.get
 import com.example.batimetriaifms.R
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-
+    lateinit var providers: List<AuthUI.IdpConfig>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+        providers = Arrays.asList<AuthUI.IdpConfig>(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.FacebookBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build()
+        )
+        showSignInOptions()
 
+        nav_view[5].setOnClickListener {
+            AuthUI.getInstance().signOut(this@MainActivity)
+                .addOnCompleteListener {
+                    nav_view[5].isEnabled = false
+                    showSignInOptions()
+                }
+                .addOnFailureListener {
+                    e -> Toast.makeText(this@MainActivity, e.message,Toast.LENGTH_SHORT).show()
+                }
 
+        }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
@@ -46,6 +73,30 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
+    private val MY_REQUEST_CODE: Int = 7117
+
+    private fun showSignInOptions(): OnCompleteListener<Void> {
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
+                providers
+            ).setTheme(R.style.MyTheme).build(), MY_REQUEST_CODE
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == MY_REQUEST_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+                Toast.makeText(this, "" + user!!.email, Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "" + response!!.error!!.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
